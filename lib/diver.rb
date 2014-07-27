56667a2f48fd4a064673037e610d9d4d2df8b1d6
Radu-Bogdan Croitoru <croitoruradubogdan@gmail.com>
2014-07-27 18:26:57 +0300

    implement version 0.0.1 of diver

--- lib/diver.rb
+++ lib/diver.rb
@@ -1,5 +1,15 @@
 require "diver/version"
 
-module Diver
-  # Your code goes here...
+class Hash
+  def dive(*args)
+    args_list = args.flatten
+    key = args_list.shift
+
+    if key
+      new_hash = self[key]
+      new_hash.class == Hash ? new_hash.dive(args_list) : new_hash
+    else
+      self
+    end
+  end
 end

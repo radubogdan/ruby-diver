0703fe4739394f4f70c0ee9184edd0441b314d61
Radu-Bogdan Croitoru <croitoruradubogdan@gmail.com>
2014-07-27 16:38:56 +0300

    adapt tests to new idea of how dive function will look like

--- test/test_diver.rb
+++ test/test_diver.rb
@@ -6,37 +6,28 @@ class DiverTest < Test::Unit::TestCase
     @short_test = {'a' => {'b' => {'c' => 'd'}}}
   end
 
-  def test_repond_to_main_function
+  def test_respond_to_main_function
     assert_respond_to(@short_test, :dive)
   end
 
   def test_expect_a_result_when_key_exist
-    assert_not_nil
-      Diver::Diver.dive(@short_test, 'a')
+    assert_not_nil(@short_test.dive('a'))
   end
 
   def test_expect_nil_as_result_when_key_doesnt_exist
-    assert_nil
-      Diver::Diver.dive(@short_test, 'x')
+    assert_nil(@short_test.dive('x'))
   end
 
   def test_should_equal_semantic
-    assert_equal @short_test['a']['b']['c']
-      Diver::Diver.dive(@short_test, 'a', 'b', 'c')
+    assert_equal(@short_test['a']['b']['c'], @short_test.dive('a', 'b', 'c'))
   end
 
   def test_should_equal_hash
-    assert_equal "{'b' => {'c' => 'd'}}"
-      Diver::Diver.dive(@short_test, 'a')
-  end
-
-  def test_should_equal_hash_test_two
-    assert_equal "{'c' => 'd'}"
-      Diver::Diver.dive(@short_test, 'a', 'b')
+    assert_equal({'b' => {'c' => 'd'}}, @short_test.dive('a'))
+    assert_equal({'c' => 'd'}, @short_test.dive('a', 'b'))
   end
 
   def test_should_equal_value
-    assert_equal 'd'
-      Diver::Diver.dive(@short_test, 'a', 'b', 'c')
+    assert_equal('d', @short_test.dive('a', 'b', 'c'))
   end
 end

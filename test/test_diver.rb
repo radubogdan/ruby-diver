dadf31ef812f8264402a1199009765d632f3529e
Radu-Bogdan Croitoru <croitoruradubogdan@gmail.com>
2014-07-30 17:42:39 +0300

    update test suite for diveSet function

--- test/test_diver.rb
+++ test/test_diver.rb
@@ -4,9 +4,11 @@ require 'diver'
 class DiverTest < Test::Unit::TestCase
   def setup
     @short_test = {'a' => {'b' => {'c' => 'd'}}}
+    @empty = {}
   end
 
-  def test_respond_to_main_function
+  # Simple tests for dive function
+  def test_respond_to_dive_function
     assert_respond_to(@short_test, :dive)
   end
 
@@ -29,5 +31,34 @@ class DiverTest < Test::Unit::TestCase
 
   def test_should_equal_value
     assert_equal('d', @short_test.dive('a', 'b', 'c'))
+  end
+
+  # Simple tests for diveSet function
+  def test_respond_to_diveSet_function
+    assert_respond_to(@empty, :diveSet)
+  end
+
+  def test_expect_something_when_string_is_given
+    assert_not_nil(@empty.diveSet('a'))
+  end
+
+  def test_expect_empty_hash
+    assert_equal(@empty, @empty.diveSet(''))
+    assert_equal({}, @empty.diveSet('a'))
+  end
+
+  def test_expect_simple_hash
+    @empty.diveSet('a', 'b')
+    assert_equal({'a' => 'b'}, @empty)
+  end
+
+  def test_expect_nested_hash
+    @empty.diveSet('a','b','c')
+    assert_equal({'a' => {'b' => 'c'}}, @empty)
+  end
+
+  def test_expect_big_nested_hash
+    @empty.diveSet('a', 'b', 'c', 'd', 'e', 'f', 'g')
+    assert_equal({'a' => {'b' => {'c' => {'d' => {'e' => {'f' => 'g'}}}}}}, @empty)
   end
 end

695fdf709e9d58ced90290c8a70a5fe5504034d0
Radu-Bogdan Croitoru <croitoruradubogdan@gmail.com>
2014-07-31 15:42:21 +0300

    add simple docummentation

--- lib/diver.rb
+++ lib/diver.rb
@@ -1,6 +1,25 @@
 require "diver/version"
 
+# Add two new methods to the Hash class that will allow ad-hoc accesses
+# of deeply nested structures
+#
+# Author: Radu-Bogdan Croitoru (mailto:croitoruradubogdan@gmail.com)
+#
+# License: MIT License. See LICENSE file.
+#
+
 class Hash
+
+  # This function will pull out one value from a nested data structure.
+  #
+  # Example:
+  #
+  # x = {"a" => {"b" => "c"}}
+  #
+  # x.dive('a')
+  #
+  # Return: {"b" => "c"}
+  #
   def dive(*args)
     args_list = args.flatten
     key = args_list.shift
@@ -13,6 +32,16 @@ class Hash
     end
   end
 
+  # This function will create a nested hash
+  #
+  # Example:
+  #
+  # x = {}
+  #
+  # x.diveSet('a','b','c')
+  #
+  # p x # {"a" => {"b" => "c"}}
+  #
   def diveSet(*args)
     args_list = args.flatten
     return self if args_list.length == 0 || args_list[0] == "" || args_list.length == 1
@@ -21,5 +50,4 @@ class Hash
     self[key] = {} unless self.has_key?(key)
     self[key].diveSet(args_list)
   end
-
 end
